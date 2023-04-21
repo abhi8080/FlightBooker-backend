32a8cb1f1792bb8b313bafc6422d1231c0b5f786
Abhinav Kalra <99359469+abhi8080@users.noreply.github.com>
2023-04-21 19:09:21 +0200

    feat: enable cors

--- Program.cs
+++ Program.cs
@@ -1,11 +1,26 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Npgsql.EntityFrameworkCore.PostgreSQL;
 using backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder => builder.WithOrigins("http://localhost:5173")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<ApplicationDbContext>(
@@ -13,8 +28,11 @@ builder.Services.AddDbContext<ApplicationDbContext>(
 
 var app = builder.Build();
 
-
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("AllowOrigin"); // Enable CORS middleware
 
 app.UseAuthorization();
 
