9b8a3de4eae155c403c5d9ec4f36c9fd2ad72460
abhi8080 <99359469+abhi8080@users.noreply.github.com>
2023-05-18 15:23:29 +0200

    chore: add env file

---
+++ env.cs
@@ -0,0 +1,8 @@
+namespace backend {
+
+    public class Env {
+        public static string connectionString = "Server=localhost;Port=5432;Database=flight_booker_db;User Id=postgres;Password=postgres;";
+    }
+
+
+}
